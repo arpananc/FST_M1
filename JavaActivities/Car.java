3b9907970260ba816978cb029f849ba1bdc491c9
arpananc <90184924+arpananc@users.noreply.github.com>
2025-02-18 21:35:14 +0530

    Add files via upload

---
+++ JavaActivities/Car.java
@@ -0,0 +1,34 @@
+package Activity1;
+
+public class Car {
+	
+	 String color="Pink";
+	 String transmission;
+	 int make;
+	 int tyres;
+	 int doors;
+	 
+	 Car()
+	 {
+		 tyres=4;
+		 doors=2;
+	 }
+	 
+	 public void displayCharacteristics() 
+	 {
+		 System.out.println("Color of the Car: " + color);
+		 System.out.println("Make of the Car: " + make);
+		 System.out.println("Transmission of the Car: " + transmission);
+		 System.out.println("Number of doors on the car: " + doors);
+		 System.out.println("Number of tyres on the car: " + tyres);
+	 }
+	 
+	 public void accelarate()
+	 {
+		 System.out.println("Car is moving forward");
+	 }
+     public void brake()
+     {
+    	 System.out.println(" Car has stopped"); 
+     }
+}
