2f9b4c231fc6804ecf430dd1a6a1c95f00665148
arpananc <90184924+arpananc@users.noreply.github.com>
2025-02-18 21:31:25 +0530

    Add files via upload

---
+++ JavaActivities/Activity1.java
@@ -0,0 +1,16 @@
+package Activity1;
+
+public class Activity1 {
+
+	public static void main(String[] args) {
+		Car i20 = new Car();
+		i20.make=2012;
+		i20.transmission="Automatic";
+		i20.color="Blue";
+		i20.displayCharacteristics();
+		i20.accelarate();
+		i20.brake();
+
+	}
+
+}
